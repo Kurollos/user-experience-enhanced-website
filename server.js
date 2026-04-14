8c2f3fdeefdbe97b3e4797f72947ec86b70604d5
Kurollos <144329418+Kurollos@users.noreply.github.com>
2026-04-14 08:23:05 +0200

    de server.js code #2

--- server.js
+++ server.js
@@ -1,5 +1,86 @@
-console.log('Hier komt je server voor Sprint 10.')
+// server.js
+import express from 'express';
+import { Liquid } from 'liquidjs';
+import path from 'path';
 
-console.log('Gebruik uit Sprint 9 alleen de code die je mee wilt nemen.')
+const app = express();
+const port = process.env.PORT || 8000;
 
-console.log('Zet \'m op!')
+// Body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Statische bestanden
+app.use(express.static('assets'));
+
+// Liquid engine
+const engine = new Liquid({
+  root: path.resolve('./views'), // hier kijkt Liquid voor index + partials
+  extname: '.liquid'
+});
+
+app.engine('liquid', engine.express());
+app.set('view engine', 'liquid'); // NIET app.set('views', ...) want dat veroorzaakt dubbele root
+
+// ====================
+// ROUTES
+// ====================
+app.get('/', async function (request, response) {
+
+ console.log(request.query)
+
+  const params = {
+    fields: "name,image,amount"
+  };
+
+  // PRICE FILTER
+  if (request.query.price) {
+    params["filter[amount][_between]"] = "0," + request.query.price;
+  }
+
+  // AGE FILTER
+  if (request.query.age) {
+    params["filter[age][_lte]"] = request.query.age;
+  }
+
+  // WHO (boyfriend / mother etc)
+  if (request.query.for) {
+    params["filter[for][_eq]"] = request.query.for;
+  }
+
+  // SEARCH TEXT
+  if (request.query.message) {
+    params["search"] = request.query.message;
+  }
+
+  // DEFAULT SORT
+  params["sort"] = "id";
+
+  const productResponse = await fetch(
+    "https://fdnd-agency.directus.app/items/milledoni_products/?" +
+    new URLSearchParams(params)
+  );
+
+  const productResponseJSON = await productResponse.json();
+
+  console.log(productResponseJSON.data);
+
+  response.render("index.liquid", {
+    products: productResponseJSON.data
+  });
+
+});
+
+
+
+
+// POST route
+app.post('/', (req, res) => {
+  res.redirect(303, '/');
+});
+
+// ====================
+// SERVER STARTEN
+// ====================
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
